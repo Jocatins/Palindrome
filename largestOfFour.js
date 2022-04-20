ca37cf59b5f0551e70753549f02523a38521fc66
Jocatins <jocakhamen@gmail.com>
2022-04-20 14:42:39 +0100

    Basic-algorithm-scripting

--- largestOfFour.js
+++ largestOfFour.js
@@ -41,3 +41,8 @@ console.log(
     [1000, 1001, 857, 1],
   ])
 );
+// Code Explanation
+// we map all items within the main array to a new array using Array.prototype.map() and return this array as the final result
+// within each inner array, we reduce its contents down to a single value using Array.prototype.reduce()
+// the callback function passed to the reduce method takes the previous value and the current value and compares the two values
+// if the current value is higher than the previous value we set it as the new previous value for comparison with the next item within the array or returns it to the map method callback if it’s the last item
